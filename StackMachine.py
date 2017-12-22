d73692c79a5e7986b62506cae31ba7f96037ff8b
Micah Grande <micah.grande@gmail.com>
2017-12-22 01:04:35 +0000

    __

--- StackMachine.py
+++ StackMachine.py
@@ -7,16 +7,16 @@ class StackMachine:
         return self.__data.pop()
     def add(self):
         if len(self.__data) > 1:
-            self.append(self.pop() + self.pop())
+            self.__append(self.pop() + self.pop())
     def sub(self):
         if len(self.__data) > 1:
-            self.append(self.pop() - self.pop())
+            self.__append(self.pop() - self.pop())
     def mul(self):
         if len(self.__data) > 1:
-            self.append(self.pop() * self.pop())
+            self.__append(self.pop() * self.pop())
     def div(self):
         if len(self.__data) > 1:
-            self.append(self.pop() / self.pop())
+            self.__append(self.pop() / self.pop())
     def mod(self):
         if len(self.__data) > 1:
-            self.append(self.pop() % self.pop())
+            self.__append(self.pop() % self.pop())
